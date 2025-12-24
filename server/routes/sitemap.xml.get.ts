f3bce919a696852592ae3ef039910b69244e858c
Kato Emmanuel <103442484+Katoemma@users.noreply.github.com>
2025-12-25 02:33:25 +0300

    feat: Add robots.txt and a dynamic sitemap generation endpoint.

--- server/routes/sitemap.xml.get.ts
+++ server/routes/sitemap.xml.get.ts
@@ -1,19 +1,19 @@
 export default defineEventHandler((event) => {
-  const siteUrl = 'https://example.com'
+  const siteUrl = 'https://katoemma.website'
   const urls = ['']
     .map((path) => `${siteUrl}/${path}`.replace(/\/$/, ''))
 
   const body = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${urls
-    .map(
-      (u) => `<url>
+      .map(
+        (u) => `<url>
     <loc>${u}</loc>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>`
-    )
-    .join('\n  ')}
+      )
+      .join('\n  ')}
 </urlset>`
 
   setHeader(event, 'Content-Type', 'application/xml; charset=utf-8')
